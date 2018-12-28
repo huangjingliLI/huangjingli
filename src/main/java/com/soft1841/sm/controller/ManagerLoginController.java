1f272749f5487e44c0a5ea07ec0dc6d67865e8d8
黄敬理 <2368046759@qq.com>
2018-12-29 00:29:59 +0800

    展示商品信息

--- src/main/java/com/soft1841/sm/controller/ManagerLoginController.java
+++ src/main/java/com/soft1841/sm/controller/ManagerLoginController.java
@@ -12,6 +12,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
